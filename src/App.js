e6589a12f9745427c75d3f01259631cbd9fc2e38
PKP RADEESHAN <140104566+RADEESHAN@users.noreply.github.com>
2024-03-26 23:47:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+
+  const navigate = useNavigate();
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to pramu</h1>
+      <button className='user-button' onClick={() => navigate('/users')}>Users</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
